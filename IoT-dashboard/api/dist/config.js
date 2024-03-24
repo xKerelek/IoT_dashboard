87de609f712421f0aefabf2a3600a87e018f6996
xKerelek <136730573+xKerelek@users.noreply.github.com>
2024-03-24 21:04:31 +0100

    Delete IoT-dashboard/api/dist directory

--- IoT-dashboard/api/dist/config.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.config = void 0;
-exports.config = {
-    port: process.env.PORT || 3100
-};
-//# sourceMappingURL=config.js.map
